4b3bf0202e4a62d34f242ffcde259ad0e3e8f719
Jiyou <jiyouleehome@gmail.com>
2021-01-05 23:59:46 +0900

    11053 (bottom up) Modify comments

--- 11053-up.cpp
+++ 11053-up.cpp
@@ -6,21 +6,20 @@ int a[1001];
 int m[1001]; // m[i] is the length of the longest increasing subsequence possible w/ elements a[1] to a[i].
 
 int main() {
-	// Get input.
 	scanf("%d",&n);
 	for (int i=1;i<=n;i++) scanf("%d",&a[i]);
 	
-	// Set base.
+	// Initialize.
 	fill(m+1,m+1+n,1);
 	
-	// Recurse.
 	for (int i=2;i<=n;i++) {
+		// Update and memo.
 		for (int j=i-1;j>=1;j--) {
 			if (a[j]<a[i]) m[i]=max(m[i],m[j]+1);
 		}
 	}
 	
-	// Finalize answer.
+	// Finalize.
 	for (int i=1;i<=n;i++) ans=max(ans,m[i]);
 		
 	printf("%d",ans);
