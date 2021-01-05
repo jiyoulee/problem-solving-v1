4d53c3bd27026f3cdf7740fa6fd86d2cce152cab
Jiyou <jiyouleehome@gmail.com>
2021-01-06 00:00:19 +0900

    19947 (bottom up)

---
+++ 19947-up.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int y;
+int m[11]; // m[i] is the maximum asset possible at year i.
+
+int main() {
+	scanf("%d%d",m,&y);
+	
+	for (int i=1;i<=y;i++) {
+		// Initialize.
+		m[i]=(int)floor(1.05*m[i-1]);
+		
+		// Update and memo.
+		if (0<=i-3) m[i]=max(m[i],(int)floor(1.20*m[i-3]));
+		if (0<=i-5) m[i]=max(m[i],(int)floor(1.35*m[i-5]));
+	}
+	
+	printf("%d",m[y]);
+	
+	return 0;
+}
