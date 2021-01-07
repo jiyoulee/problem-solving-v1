4788d2f77399d30addbfe2362004b8f4b1c4b935
Jiyou <jiyouleehome@gmail.com>
2021-01-07 20:37:42 +0900

    1927

---
+++ 1927.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, x;
+priority_queue<int,vector<int>,greater<int>> pq;
+
+int main() {
+	scanf("%d", &N);
+	
+	while (N--) {
+		scanf("%d", &x);
+		
+		if (0 == x) {
+			if (pq.empty()) {				// x is 0 and priority queue is empty.
+				printf("0\n");
+			}
+			else {							// x is 0 and priority queue is not empty.
+				printf("%d\n", pq.top());
+				pq.pop();
+			}
+		}
+		else {								// x is not 0.
+			pq.push(x);
+		}
+	}
+	return 0;
+}
