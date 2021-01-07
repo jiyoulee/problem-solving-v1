b90bee10d578f0807485b496a0eaa29fea726771
Jiyou <jiyouleehome@gmail.com>
2021-01-07 20:37:27 +0900

    11279 Modify comments

--- 11279.cpp
+++ 11279.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N,tmp;
+int N, x;
 priority_queue<int> pq;
 
 int main() {
 	scanf("%d", &N);
 	
 	while (N--) {
-		scanf("%d", &tmp);
+		scanf("%d", &x);
 		
-		if (0 == tmp) {
-			if (pq.empty()) {				// Input is 0 and priority queue is empty.
+		if (0 == x) {
+			if (pq.empty()) {				// x is 0 and priority queue is empty.
 				printf("0\n");
 			}
-			else {
-				printf("%d\n", pq.top());	// Input is 0 and priority queue is not empty.
+			else {							// x is 0 and priority queue is not empty.
+				printf("%d\n", pq.top());
 				pq.pop();
 			}
 		}
-		else {								// Input is not 0.
-			pq.push(tmp);
+		else {								// x is not 0.
+			pq.push(x);
 		}
 	}
 	
