5cb9a99e699855b530f9e51078b49995ae6dc041
Jiyou <jiyouleehome@gmail.com>
2021-01-29 23:17:29 +0900

    1929 Refactor: Replace CONTINUE with NOT + ELSE

--- math/1929.cpp
+++ math/1929.cpp
@@ -4,17 +4,16 @@ using namespace std;
 int M, N;
 int locked[1000001];
 
-int main() {														// N * log(log(N))
+int main() {										// N * log(log(N))
 	scanf("%d%d", &M, &N);
 	
 	locked[1] = true;
 	for (int i = 2; i * i <= N; i++) {
-		if (locked[i]) {
-			continue;
-		}
-		for (int j = i * i; j <= N; j += i) {
-			if (!locked[j]) {
-				locked[j] = true;
+		if (!locked[i]) {
+			for (int j = i * i; j <= N; j += i) {
+				if (!locked[j]) {
+					locked[j] = true;
+				}
 			}
 		}
 	}
