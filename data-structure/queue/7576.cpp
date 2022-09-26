d64746516e38eee2f7234306eb8368a859f07831
jiyoulee <jiyouleehome@gmail.com>
2022-09-26 15:47:43 +0900

    Perf(boj,7576): change day tracking logic
    Change logic of tracking number of days past to recursive method for better performance.
    - Integrate edge cases.
    - Reduce number of elements inserted into queue.
    - Remove terminal character.

--- data-structure/queue/7576.cpp
+++ data-structure/queue/7576.cpp
@@ -13,7 +13,6 @@ constexpr int MAX_M = 1000;
 constexpr int DIRECTION_CNT = 4;
 constexpr int dx[DIRECTION_CNT] = {-1, 1, 0, 0};
 constexpr int dy[DIRECTION_CNT] = {0, 0, -1, 1};
-constexpr int END_OF_DAY = -1;
 
 struct Tomato {
     int x;
@@ -22,11 +21,9 @@ struct Tomato {
 
 int answer;
 int M, N;
-int nx, ny;
-int ripe_square_cnt, empty_square_cnt;
+int x, y, nx, ny;
 int grid[MAX_N][MAX_M];
 int tomato_idx;
-Tomato tomato;
 queue<Tomato> ripe_tomatoes;
 
 int main(int argc, char** argv) {
@@ -35,74 +32,42 @@ int main(int argc, char** argv) {
         for (int j = 0; M > j; ++j) {
             scanf("%d", &grid[i][j]);
             if (1 == grid[i][j]) {
-                ++ripe_square_cnt;
-            }
-            else if (-1 == grid[i][j]) {
-                ++empty_square_cnt;
-            }
-        }
-    }
-    
-    if (N * M == ripe_square_cnt + empty_square_cnt) {
-        printf("0");
-        return 0;
-    }
-
-    for (int i = 0; N > i; ++i) {
-        for (int j = 0; M > j; ++j) {
-            if (1 == grid[i][j]) {
                 tomato_pool[tomato_idx].x = i;
                 tomato_pool[tomato_idx].y = j;
                 ripe_tomatoes.emplace(tomato_pool[tomato_idx++]);
             }
         }
     }
-    tomato_pool[tomato_idx].x = END_OF_DAY;
-    tomato_pool[tomato_idx].y = END_OF_DAY;
-    ripe_tomatoes.emplace(tomato_pool[tomato_idx++]);
 
     while (!ripe_tomatoes.empty()) {
-        tomato = ripe_tomatoes.front();
+        x = ripe_tomatoes.front().x;
+        y = ripe_tomatoes.front().y;
         ripe_tomatoes.pop();
 
-        if (END_OF_DAY == tomato.x) {
-            ++answer;
-
-            if (!ripe_tomatoes.empty()) {
-                ripe_tomatoes.emplace(tomato);
-            }
-        }
-        else {
-            for (int i = 0; DIRECTION_CNT > i; ++i) {
-                nx = tomato.x + dx[i];
-                ny = tomato.y + dy[i];
-
+        for (int i = 0; DIRECTION_CNT > i; ++i) {
+            nx = x + dx[i];
+            ny = y + dy[i];
                 if (0 <= nx && N > nx && 0 <= ny && M > ny && 0 == grid[nx][ny]) {
-                    ++grid[nx][ny];
-                    tomato_pool[tomato_idx].x = nx;
-                    tomato_pool[tomato_idx].y = ny;
-                    ripe_tomatoes.emplace(tomato_pool[tomato_idx++]);
-                }
+                grid[nx][ny] = grid[x][y] + 1;
+                tomato_pool[tomato_idx].x = nx;
+                tomato_pool[tomato_idx].y = ny;
+                ripe_tomatoes.emplace(tomato_pool[tomato_idx++]);
             }
         }
     }
 
-    bool is_valid = true;
     for (int i = 0; N > i; ++i) {
         for (int j = 0; M > j; ++j) {
             if (0 == grid[i][j]) {
-                is_valid = false;
-                break;
+                printf("-1");
+                return 0;
             }
+
+            answer = max(answer, grid[i][j]);
         }
     }
 
-    if (is_valid) {
-        printf("%d", answer - 1);
-    }
-    else {
-        printf("-1");
-    }
+    printf("%d", answer - 1);
 
     return 0;
 }
