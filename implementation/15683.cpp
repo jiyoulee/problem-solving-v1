2c2e205893488e08cb70df6ec95815ceac2f433f
jiyoulee <jiyouleehome@gmail.com>
2022-10-05 16:20:33 +0900

    Refactor(boj,15683): surveilence area update logic
    Pull out surveilence area update logic into a seperate function for code readability.

--- implementation/15683.cpp
+++ implementation/15683.cpp
@@ -5,6 +5,7 @@
 
 #include <cstdio>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
@@ -23,74 +24,53 @@ struct CCTV {
     int dir;
 } cctvs[MAX_CCTV_CNT];
 
-int ans, cnt;
+int ans = 65;
 int N, M;
-int cctv_cnt;
+int cctv_cnt, blindspot_cnt, temp_cnt;
 int grid[MAX_N][MAX_M];
 int temp[MAX_N][MAX_N];
 
+void survey(int x, int y, int dir) {
+    for (x += dx[dir], y += dy[dir]; 0 <= x && N > x && 0 <= y && M > y && WALL != temp[x][y]; x += dx[dir], y += dy[dir]) {
+        if (!temp[x][y]) {
+            temp[x][y] = 1;
+            ++temp_cnt;
+        }
+    }
+
+    return;
+}
+
 void go(int depth) {
     if (cctv_cnt == depth) {
-        for (int i = 0; N > i; ++i) {
-            for (int j = 0; M > j; ++j) {
-                temp[i][j] = grid[i][j];
-            }
-        }
+        memcpy(temp, grid, sizeof(grid));
 
-        int temp_cnt = 0;
+        temp_cnt = 0;
+        int type, dir;
         for (int k = 0; cctv_cnt > k; ++k) {
-            if (5 != cctvs[k].type) {
-                int dir = cctvs[k].dir;
+            type = cctvs[k].type;
+            dir = cctvs[k].dir;
 
-                for (int x = cctvs[k].x + dx[dir], y = cctvs[k].y + dy[dir]; 0 <= x && N > x && 0 <= y && M > y && WALL != temp[x][y]; x += dx[dir], y += dy[dir]) {
-                    if (!temp[x][y]) {
-                        temp[x][y] = 1;
-                        ++temp_cnt;
-                    }
-                }                
-
-                switch(cctvs[k].type) {
-                    case 2:
-                        dir = (cctvs[k].dir + 2) % 4;
-                        for (int x = cctvs[k].x + dx[dir], y = cctvs[k].y + dy[dir]; 0 <= x && N > x && 0 <= y && M > y && WALL != temp[x][y]; x += dx[dir], y += dy[dir]) {
-                            if (!temp[x][y]) {
-                                temp[x][y] = 1;
-                                ++temp_cnt;
-                            }
-                        }
-                        break;      
-                    case 4:
-                        dir = (cctvs[k].dir + 3) % 4;
-                        for (int x = cctvs[k].x + dx[dir], y = cctvs[k].y + dy[dir]; 0 <= x && N > x && 0 <= y && M > y && WALL != temp[x][y]; x += dx[dir], y += dy[dir]) {
-                            if (!temp[x][y]) {
-                                temp[x][y] = 1;
-                                ++temp_cnt;
-                            }
-                        }
-                    case 3:
-                        dir = (cctvs[k].dir + 1) % 4;
-                        for (int x = cctvs[k].x + dx[dir], y = cctvs[k].y + dy[dir]; 0 <= x && N > x && 0 <= y && M > y && WALL != temp[x][y]; x += dx[dir], y += dy[dir]) {
-                            if (!temp[x][y]) {
-                                temp[x][y] = 1;
-                                ++temp_cnt;
-                            }
-                        }
-                        break;
-                    default:
-                        break;
-                }
+            survey(cctvs[k].x, cctvs[k].y, dir);
+            if (1 != type && 2 != type) {
+                survey(cctvs[k].x, cctvs[k].y, (dir + 1) % 4);
+            }
+            if (2 == type || 5 == type) {
+                survey(cctvs[k].x, cctvs[k].y, (dir + 2) % 4);
+            }
+            if (4 == type || 5 == type) {
+                survey(cctvs[k].x, cctvs[k].y, (dir + 3) % 4);
             }
         }
 
-        cnt = max(cnt, temp_cnt);
+        ans = min(ans, blindspot_cnt - temp_cnt);
 
         return;
     }
-
-    for (int i = 0; 4 > i; ++i) {
-        cctvs[depth].dir = i;
+    
+    for (cctvs[depth].dir = 0; 4 > cctvs[depth].dir; ++cctvs[depth].dir) {
         go(depth + 1);
-    }
+    }   
 
     return;
 }
@@ -100,52 +80,19 @@ int main(int argc, char** argv) {
     for (int i = 0; N > i; ++i) {
         for (int j = 0; M > j; ++j) {
             scanf("%d", &grid[i][j]);
+
             if (!grid[i][j]) {
-                ++ans;
+                ++blindspot_cnt;
             }
             else if (WALL != grid[i][j]) {
-                cctvs[cctv_cnt].x = i;
-                cctvs[cctv_cnt].y = j;
-                cctvs[cctv_cnt++].type = grid[i][j];
-            }
-        }
-    }
-
-    for (int k = 0; cctv_cnt > k; ++k) {
-        if (5 == cctvs[k].type) {
-            int x = cctvs[k].x;
-            int y = cctvs[k].y;
-
-            for (int i = -1; 0 <= (x + i) && WALL != grid[x + i][y]; --i) {
-                if (!grid[x + i][y]) {
-                    grid[x + i][y] = 1;
-                    --ans;
-                }
-            }
-            for (int i = 1; M > (y + i) && WALL != grid[x][y + i]; ++i) {
-                if (!grid[x][y + i]) {
-                    grid[x][y + i] = 1;
-                    --ans;
-                }
-            }
-            for (int i = 1; N > (x + i) && WALL != grid[x + i][y]; ++i) {
-                if (!grid[x + i][y]) {
-                    grid[x + i][y] = 1;
-                    --ans;
-                }
-            }
-            for (int i = -1; 0 <= (y + i) && WALL != grid[x][y + i]; --i) {
-                if (!grid[x][y + i]) {
-                    grid[x][y + i] = 1;
-                    --ans;
-                }
+                cctvs[cctv_cnt++] = {i, j, grid[i][j]};
             }
         }
     }
 
     go(0);
 
-    printf("%d", ans - cnt);
+    printf("%d", ans);
 
     return 0;
 }
