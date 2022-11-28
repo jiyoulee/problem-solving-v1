8bf14e4f31f8620c740c4251a88304256b32b3e6
jiyoulee <jiyouleehome@gmail.com>
2022-11-28 20:04:51 +0900

    Add(boj,10926): initial commit

---
+++ string/10926.cpp
@@ -0,0 +1,23 @@
+/*
+ * Title: ??!
+ * Link: https://www.acmicpc.net/problem/10926
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string str;
+
+int main(int argc, char** argv) {
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+
+   cin >> str;
+
+   cout << str << "\?\?!";
+
+   return 0;
+}
