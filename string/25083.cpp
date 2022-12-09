ad1e433fbb5b6205720eac3ded62a02d2db96636
jiyoulee <jiyouleehome@gmail.com>
2022-12-09 22:33:45 +0900

    Add(boj,25083): initial commit

---
+++ string/25083.cpp
@@ -0,0 +1,14 @@
+/*
+ * Title: 새싹
+ * Link: https://www.acmicpc.net/problem/25083
+ */
+
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    printf("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+    
+    return 0;
+}
