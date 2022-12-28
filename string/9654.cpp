233bd047782ae352441b4770f09f4a61d6871378
jiyoulee <jiyouleehome@gmail.com>
2022-12-28 22:07:06 +0900

    Add(boj,9654): initial commit

---
+++ string/9654.cpp
@@ -0,0 +1,23 @@
+/*
+ * Title: Naboo Fleet Data
+ * Link: https://www.acmicpc.net/problem/9654
+ */
+
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	/*
+	 * Output.
+	 */
+   	printf("%-15s%-15s%-11s%-10s\n%-15s%-15s%-11s%-10s\n%-15s%-15s%-11s%-10s\n%-15s%-15s%-11s%-10s\n%-15s%-15s%-11s%-10s\n%-15s%-15s%-11s%-10s", 
+      "SHIP NAME", "CLASS", "DEPLOYMENT", "IN SERVICE",
+      "N2 Bomber", "Heavy Fighter", "Limited", "21",
+      "J-Type 327", "Light Combat", "Unlimited", "1",
+      "NX Cruiser", "Medium Fighter", "Limited", "18",
+      "N1 Starfighter", "Medium Fighter", "Unlimited", "25",
+      "Royal Cruiser", "Light Combat", "Limited", "4");
+
+   	return 0;
+}
