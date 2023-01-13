00663c8d76e23c93bc36ab67ded9550671f30127
jiyoulee <jiyouleehome@gmail.com>
2023-01-13 23:14:50 +0900

    Add(boj,16394): initial commit

---
+++ math/16394.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem: 홍익대학교
+ * Tags: mathematics, arithmetic
+ * Link: https://www.acmicpc.net/problem/16394
+ *
+ * Author: jiyoulee
+ * Solved: 2023-01-10
+ */
+
+#include <cstdio>
+
+using namespace std;
+
+int N;
+
+int main() {
+   /*
+    * Input.
+    */
+   scanf("%d", &N);
+
+   /*
+    * Output.
+    */
+   printf("%d", N - 1946);
+
+   return 0;
+}
