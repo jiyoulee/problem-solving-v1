f71ddd0e54516b451a04f6d12959cfb5ba22c305
jiyoulee <jiyouleehome@gmail.com>
2023-02-19 21:53:34 +0900

    Refactor(boj,11660): reduce memory complexity
    Reduce memory complexity by approximately 1MB.

--- dynamic-programming/prefix-sum/11660.cpp
+++ dynamic-programming/prefix-sum/11660.cpp
@@ -1,24 +1,44 @@
-#include <bits/stdc++.h>
+/*
+ * Problem: 구긴 힙 구하기 5
+ * Tags: dynamic-programming, prefix-sum
+ * Link: https://www.acmicpc.net/problem/11660
+ *
+ * Author: jiyoulee
+ * Solved: 2023-02-19, 2021-10-15
+ */
+
+#include <cstdio>
+
 using namespace std;
 
-int N, M, V1, H1, V2, H2;
-int a[1025][1025];
+constexpr int MAX_N = 1024;
 
-int main() {																			// O(N)
-	scanf("%d%d", &N, &M);
-	for (int v = 1; v <= N; v++) {
-		for (int h = 1; h <= N; h++) {
-			scanf("%d", &a[v][h]);
-			
-			a[v][h] += a[v][h - 1] + a[v - 1][h] - a[v - 1][h - 1];						// Calculate prefix-sum.
-		}
-	}
+int N, M;
+int A[1 + MAX_N][1 + MAX_N];
+int x1, x2, y1, y2;
 
-	for (int c = 1; c <= M; c++) {
-		scanf("%d%d%d%d", &V1, &H1, &V2, &H2);
-		
-		printf("%d\n", a[V2][H2] - a[V2][H1 - 1] - a[V1 - 1][H2] + a[V1 - 1][H1 - 1]);	// Use prefix-sum.
-	}
-	
-	return 0;
+int main() {
+    /*
+     * Input and Preset.
+     */
+    scanf("%d%d", &N, &M);
+
+    for (int i = 1; N >= i; ++i) {
+        for (int j = 1; N >= j; ++j) {
+            scanf("%d", &A[i][j]);
+
+            A[i][j] += A[i - 1][j] + A[i][j - 1] - A[i - 1][j - 1];
+        }
+    }
+
+    /*
+     * Output.
+     */
+    while (M--) {
+        scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
+
+        printf("%d\n", A[x2][y2] - A[x2][y1 - 1] - A[x1 - 1][y2] + A[x1 - 1][y1 - 1]);
+    }
+
+    return 0;
 }
