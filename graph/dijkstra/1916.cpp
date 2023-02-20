111100bd5578faf1230f279424eb60d596ad5fb2
jiyoulee <jiyouleehome@gmail.com>
2023-02-20 18:23:42 +0900

    Feat(boj,1916): change Dijkstra's Algorithm implementation
    Change implementation of Dijkstra's Algorithm to using 2D array and conducting linear search.

--- graph/dijkstra/1916.cpp
+++ graph/dijkstra/1916.cpp
@@ -1,41 +1,79 @@
-#include <bits/stdc++.h>
+/*
+ * Problem: 최소비용 구하기
+ * Tags: graph-theory, dijkstra
+ * Link: https://www.acmicpc.net/problem/1916
+ *
+ * Author: jiyoulee
+ * Solved: 2023-02-20, 2021-10-15
+ */
+
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-#define INF 987654321
+constexpr int INF = 1000000001;
+constexpr int MAX_N = 1000;
+constexpr int MAX_M = 100000;
 
-int N, M, U, V, W, S, E, u;
-vector<pair<int, int>> g[1001];
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-bool locked[1001];
-int dist[1001];
+int N, M, U, V;
+int cost[1 + MAX_N][1 + MAX_N];
+int dp[1 + MAX_N];
+int visited[1 + MAX_N];
 
 int main() {
-	scanf("%d%d", &N, &M);
-	for (int i = 1; i <= M; i++) {
-		scanf("%d%d%d", &U, &V, &W);
-		g[U].push_back(make_pair(W, V));
-	}
-	scanf("%d%d",&S, &E);
+    /*
+     * Input.
+     */
+    scanf("%d%d", &N, &M);
+    
+    for (int i = 1; N >= i; ++i) {
+        for (int j = 1; N >= j; ++j) {
+            cost[i][j] = INF;
+        }
+    }
 
-	fill(dist + 1, dist + 1000 + 1, INF);
-	dist[S]=0;
-	
-	pq.push(make_pair(dist[S], S));
-	while (!pq.empty()) {
-		u = get<1>(pq.top());
-		pq.pop();
-		
-		if (!locked[u]) {
-			locked[u] = true;
-			
-			for (auto [w,v] : g[u]) {
-				dist[v] = min(dist[v], dist[u] + w);
-				pq.push(make_pair(dist[v], v));
-			}			
-		}
-	}
-	
-	printf("%d", dist[E]);
-	
-	return 0;
+    for (int i = 0; N >= i; ++i) {
+        cost[i][i] = 0;
+        dp[i] = INF;
+    }
+
+    for (int C; M--; ) {
+        scanf("%d%d%d", &U, &V, &C);
+
+        if (cost[U][V] > C) {
+            cost[U][V] = C;
+        }
+    }
+
+    /*
+     * Compute.
+     */
+    scanf("%d%d", &U, &V);
+
+    dp[U] = 0;
+
+    for (int i = 0, node = U, node_next; N > i; node = node_next, ++i) {
+        visited[node] = true;
+
+        node_next = 0;
+
+        for (int i = 1; N >= i; ++i) {
+            if (!visited[i]) {
+                dp[i] = min(dp[i], dp[node] + cost[node][i]);
+
+                if (dp[node_next] > dp[i]) {
+                    node_next = i;
+                }
+            }
+        }
+    }
+
+    /*
+     * Output.
+     */
+    printf("%d", dp[V]);
+
+    return 0;
 }
