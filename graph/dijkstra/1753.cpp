2413f1cee05c4410e5c3a98eeb092e87f1bbab49
jiyoulee <jiyouleehome@gmail.com>
2023-02-28 17:19:44 +0900

    Perf(boj,1753): time and space complexity

--- graph/dijkstra/1753.cpp
+++ graph/dijkstra/1753.cpp
@@ -1,46 +1,76 @@
-#include <bits/stdc++.h>
+/*
+ * Problem: 최단경로
+ * Tags: graph-theory, dijkstra
+ * Link: https://www.acmicpc.net/problem/1753
+ *
+ * Author: jiyoulee
+ * Solved: 2023-02-28, 2021-10-15
+ */
+
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <functional>
+
 using namespace std;
 
-#define INF 987654321
+constexpr int MAX_V = 20000;
+constexpr int MAX_W = 10;
+constexpr int INF = 1 << 18;
 
-int N, u, M, S, U, V, W;
-vector<pair<int,int>> g[20001];
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-int dist[20001];
-int locked[20001];
+int V, E, K;
+vector<pair<int,int>> G[1 + MAX_V];
+int dp[1 + MAX_V];
+priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> min_pq;
 
 int main() {
-	scanf("%d%d%d", &N, &M, &S);
-	for (int i = 1; i <= M; i++) {
-		scanf("%d%d%d", &U, &V, &W);
-		g[U].push_back({W, V});
-	}
-	fill(dist + 1, dist + N + 1, INF);
-	
-	dist[S] = 0;
-	pq.push({dist[S], S});
-	while (!pq.empty()) {
-		u = get<1>(pq.top());
-		pq.pop();
-		
-		if (!locked[u]) {
-			locked[u] = true;
-			
-			for (auto [w,v] : g[u]) {
-				dist[v] = min(dist[v], dist[u] + w);
-				pq.push({dist[v], v});
-			}
-		}
-	}
+    /*
+     * Input.
+     */
+    scanf("%d%d%d", &V, &E, &K);
 
-	for (int i = 1; i <= N; i++) {
-		if (INF == dist[i]) {
-			printf("INF\n");
-		}
-		else {
-			printf("%d\n", dist[i]);
-		}
-	}
-	
-	return 0;
+    for (int u, v, w; E--; ) {
+        scanf("%d%d%d", &u, &v, &w);
+
+        G[u].emplace_back(v, w);
+    }
+
+    /*
+     * Compute.
+     */
+    for (int i = 1; MAX_V >= i; ++i) {
+        dp[i] = INF;
+    }
+
+    dp[K] = 0;
+    min_pq.emplace(0, K);
+
+    while (!min_pq.empty()) {
+        auto [c, u] = min_pq.top();
+
+        if (c == dp[u]) {
+            for (auto& [v, w] : G[u]) {
+                if (c + w < dp[v]) {
+                    dp[v] = c + w;
+                    min_pq.emplace(dp[v], v);
+                }
+            }
+        }
+
+        min_pq.pop();
+    }
+
+    /*
+     * Output.
+     */
+    for (int i = 1; V >= i; ++i) {
+        if (INF == dp[i]) {
+            printf("INF\n");
+        }
+        else {
+            printf("%d\n", dp[i]);
+        }
+    }
+
+    return 0;
 }
